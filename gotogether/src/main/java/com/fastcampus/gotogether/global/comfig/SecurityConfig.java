6d450d31719ff8f6c1c28102a713b81c9d9d6071
cutegyuseok <yigyuseok@gmail.com>
2023-03-08 02:22:06 +0900

    feat : Spring Security Customize

--- gotogether/src/main/java/com/fastcampus/gotogether/global/comfig/SecurityConfig.java
+++ gotogether/src/main/java/com/fastcampus/gotogether/global/comfig/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.fastcampus.gotogether.global.comfig;
 
+import com.fastcampus.gotogether.auth.jwt.JwtExceptionFilter;
+import com.fastcampus.gotogether.auth.jwt.JwtFilter;
+import com.fastcampus.gotogether.auth.jwt.JwtProperties;
+import com.fastcampus.gotogether.auth.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -21,18 +25,21 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
 
-//    private final JwtFilter jwtFilter;
-//    private final JwtProperties jwtProperties;
-//    private final JwtProvider jwtProvider;
+    private final JwtFilter jwtFilter;
+    private final JwtProperties jwtProperties;
+    private final JwtProvider jwtProvider;
 
 
     private static final String[] PUBLIC_URLS = { //이 URL은 권한 검사안함
-            "/api/register", "/api/login", "/api/index", "/api/products**", "/api/logout", "/api/products/details**","/api/refresh",
+            "/user/signup", "/user/login", "/api/logout","/user/refresh",
 
             /* swagger v3 */
             "/swagger-resources/**",
             "/v3/api-docs/**",
             "/swagger-ui/**"
+    };
+    private static final String[] ADMIN_URLS = { //이 URL은 ADMIN 권한만 허용
+            "/admin/**"
     };
 
 
@@ -52,25 +59,27 @@ public class SecurityConfig {
                 .authorizeRequests()// 다음 리퀘스트에 대한 사용권한 체크
                 .mvcMatchers(PUBLIC_URLS).permitAll() // 가입 및 인증 주소는 누구나 접근가능
                 .and()
-//                .authorizeRequests()// 다음 리퀘스트에 대한 사용권한 체크
-//                .anyRequest().authenticated()// 그외 나머지 요청은 모두 인증된 회원만 접근 가능
-//                .and()
+                .authorizeRequests()
+                .mvcMatchers(ADMIN_URLS).hasRole("ADMIN")
+                .and()
+                .authorizeRequests()// 다음 리퀘스트에 대한 사용권한 체크
+                .anyRequest().authenticated()// 그외 나머지 요청은 모두 인증된 회원만 접근 가능
+                .and()
                 .csrf().disable() // rest api이므로 csrf 보안이 필요없으므로 disable처리 // 다른곳에서는 요청을 보낼수 없게 한다. 주소 다이렉트입력
                 .httpBasic().disable() // 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
 //                .formLogin().loginPage("/login").permitAll()//로그인 기본 url 설정
 //                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증할것이므로 세션필요없으므로 생성안함.
                 .and()
-//                .addFilterBefore( // 시큐리티 필터가 작동하기전에 먼저 내가 만든 필터를 먼저 들어가서 확인해라.
-//                        // 퍼블릭 URL은 시큐리티 필터에만 해당하므로 모든 요청에 jwt 필터가 먼저 동작하게된다. addFilterbefore 로 인하여.
-//                        // 물론 퍼블릭 url은 jwt 필터만 통과하면 시큐리티필터는 예외 처리 된다.
-//                        jwtFilter, // 요청을 할때마다 한번 거쳐가는 필터.
-//                        UsernamePasswordAuthenticationFilter.class)
-//                .addFilterBefore(JwtExceptionFilter.of(jwtProvider, jwtProperties), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore( // 시큐리티 필터가 작동하기전에 먼저 내가 만든 필터를 먼저 들어가서 확인해라.
+                        // 퍼블릭 URL은 시큐리티 필터에만 해당하므로 모든 요청에 jwt 필터가 먼저 동작하게된다. addFilterbefore 로 인하여.
+                        // 물론 퍼블릭 url은 jwt 필터만 통과하면 시큐리티필터는 예외 처리 된다.
+                        jwtFilter, // 요청을 할때마다 한번 거쳐가는 필터.
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(JwtExceptionFilter.of(jwtProvider, jwtProperties), UsernamePasswordAuthenticationFilter.class)
                 .build()
                 ;
 
-        //인증을 처리하는 기본필터 UsernamePasswordAuthenticationFilter 대신 별도의 인증 로직을 가진 필터를 생성하고 사용하고 싶을 때 아래와 같이 필터를 등록하고 사용
     }
 
     @Bean
